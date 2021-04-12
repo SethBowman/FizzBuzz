4f5736c2e0dd912282941880999cdb0e81911547
Seth Bowman <brandonsethbowman@gmail.com>
2021-04-12 11:16:36 -0500

    exercise completed

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number.");
+            FizzBuzz();
+
+        
+           
+        }
+
+
+        public static void FizzBuzz()
+
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+
+
+            if (number % 3 == 0)
+            {
+                Console.Write("Buzz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.Write("Fizz");
+            }
+
+            else
+            {
+                Console.WriteLine("Not divisible by 3 or 5.");
+            }
+
+        }
+
+
+    }
+}
